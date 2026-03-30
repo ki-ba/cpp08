d0722516d095e506d6a2c3c826d9fe36eaecdade
Killian Barru <shaper.vaquero-03@icloud.com>
2026-03-30 09:35:17 +0200

    update template to force deque as subjacent data structure

--- ex02/inc/MutantStack.hpp
+++ ex02/inc/MutantStack.hpp
@@ -12,8 +12,8 @@
 
 #include <stack>
 
-template<typename T, typename Container = std::deque<T> >
-class MutantStack : public std::stack<T, Container>
+template<typename T>
+class MutantStack : public std::stack<T, std::deque<T> >
 {
 public:
 	MutantStack();
@@ -21,39 +21,39 @@ public:
 	~MutantStack();
 	MutantStack &operator=(MutantStack &other);
 
-	typedef typename Container::iterator iterator;
+	typedef typename std::deque<T>::iterator iterator;
 
 	iterator begin();
 	iterator end();
 };
 
-template <typename T, typename Container>
-MutantStack<T, Container>::MutantStack() : std::stack<T, Container>() {}
+template <typename T>
+MutantStack<T>::MutantStack() : std::stack<T>() {}
 
-template <typename T, typename Container>
-MutantStack<T, Container>::MutantStack(MutantStack &other) : std::stack<T, Container>()
+template <typename T>
+MutantStack<T>::MutantStack(MutantStack &other) : std::stack<T>()
 {
 	this->c = other.c;
 }
 
-template <typename T, typename Container>
-MutantStack<T, Container> &MutantStack<T, Container>::operator=(MutantStack &other)
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(MutantStack &other)
 {
 	this->c = other.c;
 	return (*this);
 }
 
-template <typename T, typename Container>
-MutantStack<T, Container>::~MutantStack() {}
+template <typename T>
+MutantStack<T>::~MutantStack() {}
 
-template <typename T, typename Container>
-typename MutantStack<T, Container>::iterator MutantStack<T, Container>::begin()
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
 {
 	return (this->c.begin());
 }
 
-template <typename T, typename Container>
-typename MutantStack<T, Container>::iterator MutantStack<T, Container>::end()
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
 {
 	return (this->c.end());
 }
